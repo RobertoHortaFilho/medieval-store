d32d9900545feacba52cc95e3157719c1c954c55
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-06-27 16:31:40 -0300

    fix: corrigindo ortografia

--- back-end/src/db/models/category.model.js
+++ back-end/src/db/models/category.model.js
@@ -2,7 +2,7 @@ const CategoryModel = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
     id: {
       type: DataTypes.INTEGER,
-      autoIncremente: true,
+      autoIncrement: true,
       allowNull: false,
       primaryKey: true,
     },
