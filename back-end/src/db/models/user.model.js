83dd3bce93a27edd4f043daddd5ba38b6d88430d
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-06-29 01:23:45 -0300

    feat: model user

---
+++ back-end/src/db/models/user.model.js
@@ -0,0 +1,37 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    alias: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    photo: {
+      type: DataTypes.STRING,
+    },
+    permission: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    tableName: 'users',
+    timestamps: false,
+
+  })
+
+  return User
+}
+
+module.exports = UserModel;
