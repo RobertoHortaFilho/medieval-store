3f861ae134c509201086a77a558d4d0a49b1e7c4
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-06-30 03:41:46 -0300

    feat: implementando rota de autenticaçao

--- back-end/src/Routes/users/index.js
+++ back-end/src/Routes/users/index.js
@@ -1,8 +1,10 @@
 const Express = require('express')
 const userController = require('../../controller/user')
+const { decodeTokenMiddleware } = require('../../utils/jwt')
 
 const route = Express.Router()
 
 route.post('/login', userController.login)
+route.post('/auth', decodeTokenMiddleware, userController.auth)
 
 module.exports = route
--- back-end/src/controller/user/index.js
+++ back-end/src/controller/user/index.js
@@ -9,4 +9,15 @@ const login = async (req, res) => {
   return res.status(STATUS.OK).json(data)
 }
 
-module.exports = { login }
+const auth = async (req, res) => {
+  const { name, permission } = req.user
+  const dataUserDb = userService.getUser(name)
+  if (dataUserDb)
+    return res.status(200).json({
+      permission,
+      status: true
+    })
+  return res.status(401).json({message: 'Invalid token', status: false })
+}
+
+module.exports = { login, auth }
